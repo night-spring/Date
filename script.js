9eacbd70da23d25e4452d187f474e6aa972c5f25
Debojit Roy <139033817+night-spring@users.noreply.github.com>
2023-12-18 15:10:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function showMeet() {
+    document.getElementById("yes").style.display = "none";
+    document.getElementById("no").style.display = "none";
+    document.getElementById("question").style.display = "none";
+    document.getElementById("meet").style.display = "block";
+}
+
+function relocateNo() {
+    const min = 0;
+    const max = window.innerHeight - 50;
+    const x = Math.floor(Math.random() * (max - min + 1)) + min;
+    const y = Math.floor(Math.random() * (max - min + 1)) + min;
+    document.getElementById("no").style.position = "absolute";
+    document.getElementById("no").style.top = x + "px";
+    document.getElementById("no").style.left = y + "px";
+}
